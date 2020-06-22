0c137e0c535cc8d6aa1ed883a66f538844033295
Taylor Somers <tsomers@yandex.com>
2020-06-22 18:21:41 -0400

    Change title of HtmlWebpackPlugin to title of current project

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Ping Pong',
+      title: 'journal',
       template: './src/index.html',
       inject: 'body'
     })
